6f4466030d4d603af103b6fdb4a7842f635a54f8
Sean McAuliffe <sean@seanmcauliffe.ca>
2025-06-08 00:10:38 -0700

    Add build configuration

---
+++ eleventy.config.js
@@ -0,0 +1,58 @@
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+const date = require("./utils/date.js");
+const code = require("./utils/code.js");
+const utils = require("./utils/util.js");
+
+module.exports = function(eleventyConfig) {
+
+  // Collections
+  eleventyConfig.addCollection("blog", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/blog/*.md").filter(item => {
+      return !item.inputPath.endsWith("index.md");
+    });
+  });
+  eleventyConfig.addCollection("project", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/projects/*.md").filter(item => {
+      return !item.inputPath.endsWith("index.md");
+    });
+  });
+
+  // Filters
+  eleventyConfig.addFilter("formatDate", date.format);
+  eleventyConfig.addFilter("formatLong", date.formatLong);
+  eleventyConfig.addFilter("lastModified", date.lastModified);
+  eleventyConfig.addFilter("createdOn", date.createdOn);
+
+  // Passthroughs
+  eleventyConfig.addPassthroughCopy({"static/" : "static/"})
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/highlight.js/styles/github-dark-dimmed.css": "static/styles/github-dark-dimmed.css"
+  });
+
+  // Watchers, update dev server when these files are edited
+  eleventyConfig.addWatchTarget("static/styles/bundle.css")
+
+  // Code syntax highlighting, markdown attributes
+  eleventyConfig.setLibrary("md", code.highlight());
+
+  // Short Codes
+  eleventyConfig.addShortcode("tweet", code.tweet);
+ 
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site"
+    },
+    markdownTemplateEngine: "liquid", // Enables {{ content }} in HTML layout
+    htmlTemplateEngine: "liquid",
+    templateFormats: [
+      "md",
+      "html"
+    ],
+    ignores: ["README.md"],
+  };
+};
+
